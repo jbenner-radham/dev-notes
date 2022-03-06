4a14932410fe540be8525267802eb5624578e707
James Benner <james.benner@gmail.com>
2022-03-06 17:38:47 -0500

    Add MD editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import React, { KeyboardEventHandler } from 'react';
+import showdown from 'showdown';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 import './App.css';
+
+const Editor = styled(Paper)(({ theme }) => ({
+    color: theme.palette.text.secondary,
+    padding: theme.spacing(1)
+}));
+
+const Preview = styled(Paper)(({ theme }) => ({
+    color: theme.palette.text.secondary,
+    padding: theme.spacing(1)
+}));
+
+function convert() {
+    const md = (document.getElementById('App__editor') as HTMLDivElement).innerText;
+    console.log({ md })
+    const target = document.getElementById('App__preview')
+    const converter = new showdown.Converter();
+
+    converter.setFlavor('github');
+
+    const html = converter.makeHtml(md);
+
+    target!.innerHTML = html;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <main><div style={{ backgroundColor: 'tomato', minHeight: '50vh' }} contentEditable></div></main>
+        <main>
+            <Stack direction="row" spacing={2}>
+                {/* <Box component="div" sx={{ border: 5, borderColor: 'primary.main', minHeight: 'calc(50vh - 1rem)', width: '48%', padding: '2rem' }} contentEditable></Box> */}
+                <Editor id="App__editor" sx={{ height: 'calc(100vh - 3rem)', width: '48%' }} onKeyUp={convert} contentEditable></Editor>
+                <Preview id="App__preview" sx={{ height: 'calc(100vh - 3rem)', padding: '2rem', textAlign: 'left', width: '48%' }}></Preview>
+            </Stack>
+        </main>
     </div>
   );
 }
