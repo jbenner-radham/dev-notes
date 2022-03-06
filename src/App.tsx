4a36af3ec92ba52c103594f5330ae769b190a657
James Benner <james.benner@gmail.com>
2022-03-06 16:28:41 -0500

    Setup and experimenting

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <main><div style={{ backgroundColor: 'tomato', minHeight: '50vh' }} contentEditable></div></main>
     </div>
   );
 }
